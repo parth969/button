0855ba8f2b83f4cbf9b7b263074c308013e85940
COE-026 <coe-026@coe-026s-imac.local>
2019-08-22 13:40:01 +0530

    Initial Commit

---
+++ button_textfillexTests/button_textfillexTests.swift
@@ -0,0 +1,36 @@
+//
+//  button_textfillexTests.swift
+//  button_textfillexTests
+//
+//  Created by COE-026 on 22/08/19.
+//  Copyright © 2019 COE-026. All rights reserved.
+//
+
+import XCTest
+@testable import button_textfillex
+
+class button_textfillexTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
